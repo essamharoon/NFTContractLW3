b3d9cdf252abae8332398455e29aae8460917ddd
essamharoon <eh4@me.com>
2022-05-04 16:22:42 +0300

    master

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+
+const WHITELIST_CONTRACT_ADDRESS = "0xaad93083Ae4c2B190cA528B26eC4dA180be90D8A";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
